b9ed2362e55a92b644a5ed5cf4546d3d4381fc47
Bruno Nogueira Martins <bn.martins02@gmail.com>
2022-05-18 16:18:19 -0300

    Implementação final do menu do fotografo

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,51 @@
-var  btnMenu = document.querySelector("#menu-toggle");
+var btnMenu = document.querySelector("#menu-toggle");
 var menuPrincipal = document.querySelector("#menu-principal");
 
-btnMenu.addEventListener("click", function (){
+var home = document.querySelector(".home");
+var personalizacao = document.querySelector(".personalizacao");
+var sFotos = document.querySelector(".sele-fotos");
+var cliente = document.querySelector(".cliente");
+var credito = document.querySelector(".credito");
+var cartao = document.querySelector(".cartao");
+var pacote = document.querySelector(".pacote");
+var suporte = document.querySelector(".suporte");
+var conta = document.querySelector(".conta");
+var config = document.querySelector(".configuracao");
+var sair = document.querySelector(".sair");
+
+if(window.matchMedia("min-width:992px")){
+    menuPrincipal.classList.add("ativo");
+    nameMenu();
+}
+
+btnMenu.addEventListener("click", function (event) {
     menuPrincipal.classList.toggle("ativo");
+    nameMenu();
 });
+function nameMenu(){
+    if (home.textContent === "") {
+        home.textContent = "Home";
+        personalizacao.textContent = "Personalização";
+        sFotos.textContent = "Seleção de Fotos";
+        cliente.textContent = "Clientes";
+        credito.textContent = "Ganhar Créditos";
+        cartao.textContent = "Cartão Fidelidade";
+        pacote.textContent = "Pacote e Valores";
+        suporte.textContent = "Suporte";
+        conta.textContent = "Conta";
+        config.textContent = "Configurações";
+        sair.textContent = "Sair";
+    }else{
+        home.textContent = "";
+        personalizacao.textContent = "";
+        sFotos.textContent = "";
+        cliente.textContent = "";
+        credito.textContent = "";
+        cartao.textContent = "";
+        pacote.textContent = "";
+        suporte.textContent = "";
+        conta.textContent = "";
+        config.textContent = "";
+        sair.textContent = "";
+    }
+}
